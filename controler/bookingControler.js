ac2cadf8eaae49b1f70edb3c30c0723dd937346c
jayesh khuman <isquare.jayesh@gmail.com>
2023-01-17 18:47:27 +0530

    first commit

--- controler/bookingControler.js
+++ controler/bookingControler.js
@@ -58,7 +58,7 @@ exports.getChekOutSession =catchAsync( async(req, res, next) => {
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.display_items[0].amount / 100;
+    const price = session.object.amount_total / 100;
     await booking.create({ tour, user, price });
   };
 
